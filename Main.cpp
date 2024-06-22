ee886e005d5990c6556c9df80b632d8768dd0aa8
Isaac  Appiagyei <ikwaku465@gmail.com>
2024-06-22 07:38:08 +0200

    Add files via upload
    add some notes to make the code more discreptive

--- Main.cpp
+++ Main.cpp
@@ -2,13 +2,14 @@
 #include <string>
 using namespace std;
 
+// Here you can see that the same function has been overlaoded 3 times 
 void Print(string str);
 void Print(int num);
 void Print(string str1, string str2);
 
 int main()
 {
-
+	// Function overload 
 	Print("Hello people");
 	Print(4);
 	Print("Hi there", "Come on!");
